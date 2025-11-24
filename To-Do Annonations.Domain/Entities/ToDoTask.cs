844a814aaee63ae1a3012f38a1da1678fb8e47b3
IdkJoa <146264429+JoaquinErnestoMeloNunez@users.noreply.github.com>
2025-11-24 17:20:45 -0400

    Feature: Agregando la estructura base y configurando el acceso a la base de datos.

---
+++ To-Do Annonations.Domain/Entities/ToDoTask.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskStatus = To_Do_Annonations.Domain.Enums.TaskStatus;
+
+namespace To_Do_Annonations.Domain.Entities
+{
+    public class ToDoTask
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You must add a title. It's mandatory.")]
+        [StringLength(100, ErrorMessage = "The title cannot exceed 100 characters.")]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.NotStarted;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ To-Do Annonations.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace To_Do_Annonations.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        NotStarted = 0,
+        Pending = 1,
+        Completed = 2
+    }
+}
